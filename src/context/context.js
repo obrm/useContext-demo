81618e89b6a712d9d94fc4807c712e08cfb10ff5
Ori Baram <obrm770@gmail.com>
2023-02-26 12:21:13 +0200

    refactor

--- src/context/context.js
+++ src/context/context.js
@@ -9,18 +9,22 @@ const AppProvider = ({ children }) => {
   const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
   const [page, setPage] = useState({ page: '', links: [] });
   const [location, setLocation] = useState({});
+
   const openSidebar = () => {
     setIsSidebarOpen(true);
   };
+
   const closeSidebar = () => {
     setIsSidebarOpen(false);
   };
+
   const openSubmenu = (text, coordinates) => {
     const page = sublinks.find((link) => link.page === text);
     setPage(page);
     setLocation(coordinates);
     setIsSubmenuOpen(true);
   };
+
   const closeSubmenu = () => {
     setIsSubmenuOpen(false);
   };
@@ -42,7 +46,7 @@ const AppProvider = ({ children }) => {
     </AppContext.Provider>
   );
 };
-// make sure use
+
 export const useGlobalContext = () => {
   return useContext(AppContext);
 };
